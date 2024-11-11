eb904cf97c7a687c3d316a39926eff621099eab7
Manuel <mlc280803@gmail.com>
2024-11-11 13:28:17 +0100

    Creados los métodos getter y setter de los atributos fila y columna.

--- src/main/java/org/iesalandalus/programacion/damas/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -4,4 +4,28 @@ public class Posicion {
 
     private int fila;        // Atributo para la fila (1-8)
     private char columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila)
+    {
+        if (fila < 1 || fila > 8)
+            throw new IllegalArgumentException("ERROR: La fila debe estar entre 1 y 8.");
+
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna)
+    {
+        if (columna < 'a' || columna > 'h')
+            throw new IllegalArgumentException("ERROR: La columna debe estar entre la 'a' y la 'h'.");
+
+        this.columna = columna;
+    }
 }
