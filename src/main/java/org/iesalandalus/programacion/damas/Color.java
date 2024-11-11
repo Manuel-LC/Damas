4252035876f31a431c3f79ec4e1f17e1f1830066
Manuel <mlc280803@gmail.com>
2024-11-11 12:48:57 +0100

    Creado el constructor dentro del enumerado Color.

--- src/main/java/org/iesalandalus/programacion/damas/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/Color.java
@@ -4,4 +4,8 @@ public enum Color {
     BLANCO("Blanco"), NEGRO("Negro");
 
     private final String cadenaAMostrar;
+
+    private Color(String cadena) {
+        this.cadenaAMostrar = cadena;
+    }
 }
