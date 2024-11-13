9aefd340116c341819860703d4a454756391fdf7
Manuel <mlc280803@gmail.com>
2024-11-13 12:28:37 +0100

    Creados los métodos getter y setter en la clase Dama.

--- src/main/java/org/iesalandalus/programacion/damas/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/Dama.java
@@ -5,4 +5,34 @@ public class Dama {
     private Color color;
     private Posicion posicion;
     private boolean esDamaEspecial;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if (color == null)
+            throw new NullPointerException("ERROR: El color no es válido.");
+
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        if (posicion == null)
+            throw new NullPointerException("ERROR: La posición no es válida.");
+
+        this.posicion = posicion;
+    }
+
+    public boolean getEsDamaEspecial() {
+        return esDamaEspecial;
+    }
+
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
 }
